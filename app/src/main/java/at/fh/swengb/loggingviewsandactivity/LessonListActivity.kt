d4cef3ba0c13944799154456c2afd172c5c6ee17
mattlsch18 <mattl_2612@hotmail.com>
2019-11-15 10:45:52 +0100

    jetzt steppt der bär aber richtig

--- app/src/main/java/at/fh/swengb/loggingviewsandactivity/LessonListActivity.kt
+++ app/src/main/java/at/fh/swengb/loggingviewsandactivity/LessonListActivity.kt
@@ -1,12 +1,17 @@
 package at.fh.swengb.loggingviewsandactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_lesson_list.*
 
 class LessonListActivity : AppCompatActivity() {
-    val lessonAdapter = LessonAdapter()
+    val lessonAdapter = LessonAdapter() {
+        val intent = Intent(this, LessonRatingActivity::class.java)
+        startActivity(intent)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
